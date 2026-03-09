09c744a8c9afe7572f7b5476f1d0592f817eb418
arunchauhan02 <100798589+arunchauhan02@users.noreply.github.com>
2026-03-09 18:58:24 +0530

    Parameterize git clone URL and branch

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,4 +1,4 @@
 def call(string url, string branch){
   echo "Git cloning"
-  git url:"https://github.com/arunchauhan02/ProjectSellBackend.git",branch:"main"
+  git url:"${url}",branch:"${branch}"
 }
